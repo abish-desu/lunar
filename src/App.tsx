ee3ebd0835dc114ec7b5d5a877ccafe52a981802
Abish-x-lethal <magarabish456@gmail.com>
2023-06-16 09:49:27 +0545

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home/Home'
-import Navbar from './components/Navbar/Navbar'
-import Rooms from './components/Rooms/Rooms'
-import Footer from './components/Footer'
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Rooms from "./components/Rooms/Rooms";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/rooms' element={<Rooms />} />
-         
-
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<Rooms />} />
         </Routes>
         <Footer />
       </Router>
-
-
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
